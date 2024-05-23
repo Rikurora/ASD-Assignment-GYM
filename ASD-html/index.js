a305fe562d1c2cb2b863186d0157b445bd948d6e
Rikurora Karamata <rikuukaramata@gmail.com>
2024-05-24 00:56:14 +0200

    Added ads

---
+++ ASD-html/index.js
@@ -0,0 +1,22 @@
+var formdata = new FormData();
+formdata.append("type", "Horizontal Strip");
+formdata.append("tags", "gym");
+
+var ajax = new XMLHttpRequest();
+ajax.addEventListener("load", completeHandler, false);
+
+ajax.open("POST", "https://ad.simaneka.com/api/get");
+ajax.setRequestHeader("authorisation", "4OZR8NUgJyBNDePmdsinWNDp4ebZQzRc");
+
+ajax.send(formdata);
+
+function completeHandler(event) {
+    var response = JSON.parse(event.target.responseText);
+
+    console.log(response);
+    document.querySelector('.advertIMG').src = response.link;
+    document.querySelector('.advertIMG').alt = response.alt;
+    document.querySelector('.anchorElement').href = response.href;
+    document.querySelector('.headerText').innerHTML = response.message;
+}
+                                 
